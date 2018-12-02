a0a6bf6631bbcf887700afee67779b7cf49a6a15
Tutti Quintella <q.tuane@gmail.com>
2018-12-02 19:02:16 +0900

    add url shortcut

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: 'urls#new'
   resources :urls, param: :slug
+
+  get '/:slug', to: 'urls#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
