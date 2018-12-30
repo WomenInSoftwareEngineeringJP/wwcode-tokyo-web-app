64645ccb7028b611811b8be090b4a42364945406
Tutti Quintella <q.tuane@gmail.com>
2018-12-30 11:20:23 -0800

    change app path

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :pty,             true
 set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
-set :deploy_to,       "/var/www/#{fetch(:application)}"
+set :deploy_to,       "/home/#{fetch(:user)}/www/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
